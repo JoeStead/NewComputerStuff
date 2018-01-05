9aad80ddf7182d4009765b52ceaa0ba9424a5d7d
Joe Stead <joestead1@gmail.com>
2018-01-05 13:25:13 +0000

    Added a brew list that I can sort of think of right now

---
+++ brew-mac.sh
@@ -0,0 +1,9 @@
+brew install ccat
+brew install elixir
+brew install git
+brew install hub
+brew install mono
+brew install yarn
+brew install htop
+brew install scriptcs
+brew cask install java
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Installing Stuff
+My windows machine died and I don't have an easy way to bring it back online, so I'm gonna start creating scripts for my different OS setups.
